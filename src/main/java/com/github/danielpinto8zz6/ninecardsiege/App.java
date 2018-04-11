a9efe4bcda001dc60f1855106b66cd3df4a67fdc
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2018-04-11 01:19:02 +0100

    first commit

---
+++ src/main/java/com/github/danielpinto8zz6/ninecardsiege/App.java
@@ -0,0 +1,9 @@
+package com.github.danielpinto8zz6.ninecardsiege;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
