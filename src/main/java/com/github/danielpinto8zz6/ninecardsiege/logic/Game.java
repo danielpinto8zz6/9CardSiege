10a1792a696bbceb99e5bf6acdea8a0607761b4e
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2018-04-16 18:37:52 +0100

    Add more methods

--- src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/Game.java
+++ src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/Game.java
@@ -4,13 +4,18 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.danielpinto8zz6.ninecardsiege.logic.states.IStates;
+
 public class Game implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private List<Player> players = new ArrayList<Player>();
 
+	private IStates state;
+
 	public Game() {
+
 	}
 
 	public void setupGame() {
@@ -20,4 +25,24 @@ public class Game implements Serializable {
 		players.add(new Player(name));
 	}
 
+	/**
+	 * @return the players
+	 */
+	public List<Player> getPlayers() {
+		return players;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public IStates getState() {
+		return state;
+	}
+
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(IStates state) {
+		this.state = state;
+	}
 }
