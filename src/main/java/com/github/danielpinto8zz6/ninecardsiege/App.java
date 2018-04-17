166a6de191e4cc3c82b419ef51546bfaf9299112
tiago_000 <tiago_000@192.168.1.42>
2018-04-17 17:31:09 +0100

    adição de algumas classes, incompletas

--- src/main/java/com/github/danielpinto8zz6/ninecardsiege/App.java
+++ src/main/java/com/github/danielpinto8zz6/ninecardsiege/App.java
@@ -9,5 +9,6 @@ public class App {
     public static void main(String[] args) {
         TextUI textUI = new TextUI(new Game());
         textUI.run();
+        System.out.println("did  something\n");
     }
 }
