f621cdfa08e880b2e56a386121f1d289e09cb7b9
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2018-04-18 15:09:06 +0100

    Add more cards + shuffle cards

---
+++ src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/cards/Card4.java
@@ -0,0 +1,10 @@
+package com.github.danielpinto8zz6.ninecardsiege.logic.cards;
+
+import com.github.danielpinto8zz6.ninecardsiege.logic.Card;
+
+public class Card4 extends Card {
+    private static final long serialVersionUID = 1L;
+
+	public Card4() {
+    }
+}
