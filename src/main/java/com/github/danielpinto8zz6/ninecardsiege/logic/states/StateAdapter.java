d6a82f4c2d1c2cb503a859cf1b4e169d296d29d8
tiago_000 <tiago_000@192.168.1.42>
2018-04-24 02:25:32 +0100

    ...

--- src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/states/StateAdapter.java
+++ src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/states/StateAdapter.java
@@ -17,11 +17,37 @@ public class StateAdapter implements IStates, com.github.danielpinto8zz6.ninecar
         this.game = game;
     }
 
-    public IStates startGame() {
-        return null;
-    }
+    @Override
+    public IStates startGame(){ return this; }
 
     public IStates quit() {
         return null;
+    }
+
+    public IStates pickAnEventCard() {
+        return this;
+    }
+    public IStates doEvent() {
+        return  this;
+    }
+
+    public IStates moveEnemies() {
+    return  this;
+    }
+
+    public IStates checkPlayerAtion() {
+    return  this;
+    }
+
+    public IStates playerAtion() {
+    return  this;
+    }
+
+    public IStates endOfTheDay() {
+return  this;
+    }
+
+    public IStates endChek() {
+return  this;
     }
 }
