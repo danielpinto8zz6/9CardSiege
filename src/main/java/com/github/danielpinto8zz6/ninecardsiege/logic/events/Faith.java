f1d081c20e4cffccba432b532141c4a154a516c5
tiago_000 <tiago_000@192.168.1.42>
2018-04-24 16:41:33 +0100

    modificar eventos

--- src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/events/Faith.java
+++ src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/events/Faith.java
@@ -1,8 +1,22 @@
 package com.github.danielpinto8zz6.ninecardsiege.logic.events;
 
+import com.github.danielpinto8zz6.ninecardsiege.logic.Enemy;
 import com.github.danielpinto8zz6.ninecardsiege.logic.GameData;
+import com.github.danielpinto8zz6.ninecardsiege.logic.Player;
 
 public class Faith {
+
     public static void apply(GameData gameData) {
+        Player player = gameData.getPlayer();
+        player.setmRaidSupplies(player.getmRaidSupplies() + 1);
+
+        Enemy enemyR = gameData.getEnemy("BattleRam");
+        Enemy enemyL = gameData.getEnemy("Ladders");
+        if (enemyR != null) {
+            enemyR.setStrength(enemyR.getStrength() - 1);
+        }
+        if (enemyL != null) {
+            enemyL.setStrength(enemyL.getStrength() - 1);
+        }
     }
 }
