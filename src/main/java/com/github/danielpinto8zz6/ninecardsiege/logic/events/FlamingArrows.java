9660303a999554750661300b00370e47369728b7
tiago_000 <tiago_000@192.168.1.42>
2018-04-24 16:47:54 +0100

    modificar eventos

--- src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/events/FlamingArrows.java
+++ src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/events/FlamingArrows.java
@@ -1,8 +1,13 @@
 package com.github.danielpinto8zz6.ninecardsiege.logic.events;
 
+import com.github.danielpinto8zz6.ninecardsiege.logic.Enemy;
 import com.github.danielpinto8zz6.ninecardsiege.logic.GameData;
 
 public class FlamingArrows {
     public static void apply(GameData gameData) {
+        Enemy enemy = gameData.getEnemy("SiegeTower");
+                if (enemy != null) {
+            enemy.setStrength(enemy.getStrength() - 1);
+        }
     }
 }
