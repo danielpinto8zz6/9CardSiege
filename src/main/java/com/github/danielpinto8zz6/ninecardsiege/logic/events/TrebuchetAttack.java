5b323fdb37d9e0a21a6f271c19a5d3a8d74d5182
tiago_000 <tiago_000@10.65.133.51>
2018-05-12 16:27:56 +0100

    not working as entended

--- src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/events/TrebuchetAttack.java
+++ src/main/java/com/github/danielpinto8zz6/ninecardsiege/logic/events/TrebuchetAttack.java
@@ -1,5 +1,6 @@
 package com.github.danielpinto8zz6.ninecardsiege.logic.events;
 
+import com.github.danielpinto8zz6.ninecardsiege.logic.Dice;
 import com.github.danielpinto8zz6.ninecardsiege.logic.GameData;
 
 /**
@@ -16,6 +17,20 @@ public class TrebuchetAttack {
    * @param gameData a {@link com.github.danielpinto8zz6.ninecardsiege.logic.GameData} object.
    */
   public static void apply(GameData gameData) {
-    gameData.getPlayer().setWallStrength(gameData.getPlayer().getWallStrength() - 2);
+    switch (gameData.getBattleCard().getTrebuchet()) {
+      case 3:
+        gameData.getPlayer().setWallStrength(gameData.getPlayer().getWallStrength() - 2);
+        break;
+      case 2:
+        gameData.getPlayer().setWallStrength(gameData.getPlayer().getWallStrength() - 1);
+        break;
+      case 1:
+        int roll = Dice.roll();
+        if (roll > 3)
+          gameData.getPlayer().setWallStrength(gameData.getPlayer().getWallStrength() - 1);
+        break;
+      default:
+        break;
+    }
   }
 }
